bbc74ea68f714d839622667f3e6a268c40ed1b67
Korey Kinney <kkinney2@student.fitchburgstate.edu>
2019-02-13 19:20:41 -0500

    Scripting Scene
    Work started on player controller and enemy spawning

---
+++ Team Clinic 1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+    public float angleModX = 20;
+    public float angleModY = 20;
+    public float maxViewAngle = 5f;
+    public float minViewAngle = 5f;
+
+    private Vector3 offset;
+    private float xPos;
+    private float yPos;
+    private float viewY;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = player.transform.position + offset;
+        transform.RotateAround(point: player.transform.position, axis: Vector3.up, angle: angleModX * Input.GetAxis("Mouse X"));
+        
+        viewY += Input.GetAxis("Mouse Y");
+        if(viewY <= maxViewAngle || viewY >= minViewAngle)
+        {
+            if(viewY == maxViewAngle && Input.GetAxis("Mouse Y") < 0)
+            {
+                transform.RotateAround(point: player.transform.position, axis: transform.TransformVector(Vector3.left), angle: angleModY * Input.GetAxis("Mouse Y"));
+            }
+
+            else if (viewY == minViewAngle && Input.GetAxis("Mouse Y") > 0)
+            {
+                transform.RotateAround(point: player.transform.position, axis: transform.TransformVector(Vector3.left), angle: angleModY * Input.GetAxis("Mouse Y"));
+            }
+            else
+            {
+                transform.RotateAround(point: player.transform.position, axis: transform.TransformVector(Vector3.left), angle: angleModY * Input.GetAxis("Mouse Y"));
+            }
+        }
+    }
+    
+    void LateUpdate()
+    {
+        offset = transform.position - player.transform.position;
+        //transform.LookAt(player.transform.position);
+    }
+}
---
+++ Team Clinic 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 10;
+    public float jumpForce = 5;
+    public float angleMod = 20;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+    public GameObject moveTarget;
+
+    private Rigidbody rb;
+    private Vector3 startCoord;
+    private bool isJumping;
+    private Vector3 m_Velocity = Vector3.zero;
+    private float m_MovementSmoothing = .05f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startCoord = transform.position;
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Move(moveHorizontal * Time.fixedDeltaTime, moveVertical * Time.fixedDeltaTime);
+
+        //Jumping Mechanic
+        if (Input.GetButtonDown(buttonName: "Jump") && !isJumping)
+        {
+            Jump();
+        }
+
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1);
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1);
+        }
+
+        //Fall Death/Respawn
+        if (transform.position.y < -10)
+        {
+            transform.position = startCoord;
+        }
+    }
+
+    void Move(float moveX, float moveZ)
+    {
+        Vector3 targetVelocity = new Vector3(moveX, 0.0f, moveZ);
+        targetVelocity = moveTarget.transform.TransformVector(targetVelocity) * speed;
+        rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        //rb.AddForce(targetVelocity * speed);
+    }
+
+    void Jump()
+    {
+        rb.AddForce((Vector3.up * jumpForce), ForceMode.Impulse);
+        isJumping = true;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+}
